be21b2d0f8012d29ba55b988d8e55c7d10b56963
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-29 15:14:09 +0100

    completed add_two_numbers method

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -13,9 +13,17 @@
 def add_two_numbers(l1, l2)
   p l1 = l1.reverse.join.to_i
   p l2 = l2.reverse.join.to_i
-  p sum_l = (l1 + l2).to_s.split('')
+  p sum_l = (l1 + l2).to_s.split('').reverse
 end
 
 l1 = [2,4,3]
 l2 = [5,6,4]
+add_two_numbers(l1, l2)
+
+l1 = [0]
+l2 = [0]
+add_two_numbers(l1, l2)
+
+l1 = [9,9,9,9,9,9,9]
+l2 = [9,9,9,9]
 add_two_numbers(l1, l2)
