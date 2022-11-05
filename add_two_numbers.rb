ae6fc57bc5f2e79b76f97834ea0a9992a8ac76c4
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-05 16:40:10 +0000

    created current and result to store ListNode for calculations

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -12,9 +12,13 @@
 
 def add_two_numbers(l1, l2)
   # Return ListNode if the cases of either l1, l2 is nil or both of them are nil
-  return ListNode.new(0) if l1.nil? && l2.nil?
+  return nil if l1.nil? && l2.nil?
   return l1 if l2.nil?
   return l2 if l1.nil?
+
+  current = ListNode.new()
+  result = current
+  carry = false
 
 
 end
