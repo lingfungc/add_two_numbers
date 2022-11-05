7ac424bdfa8c221bb3de9fd6405badb5393c9587
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-05 15:47:18 +0000

    Return ListNode for cases that l1, l2, or both are nil

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -11,30 +11,10 @@
 # @return {ListNode}
 
 def add_two_numbers(l1, l2)
-  node1 = l1
-  node2 = l2
-  carry = false
-  head_int = node1.var + node2.var
+  # Return ListNode if the cases of either l1, l2 is nil or both of them are nil
+  return ListNode.new(0) if l1.nil? && l2.nil?
+  return l1 if l2.nil?
+  return l2 if l1.nil?
 
-  if head_int > 9
-    carry = true
-    head_int -= 10
-  end
 
-  root = ListNode.new(head_int)
-  last = root
-  node1 = node1.next
-  node2 = node2.next
 end
-
-l1 = [2,4,3]
-l2 = [5,6,4]
-p add_two_numbers(l1, l2)
-
-l1 = [0]
-l2 = [0]
-p add_two_numbers(l1, l2)
-
-l1 = [9,9,9,9,9,9,9]
-l2 = [9,9,9,9]
-p add_two_numbers(l1, l2)
