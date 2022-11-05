50c90f068fc154179687861b53cfe6ed341ee588
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-05 16:51:56 +0000

    added psuedocode for creating ListNode at first

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -16,18 +16,25 @@ def add_two_numbers(l1, l2)
   return l1 if l2.nil?
   return l2 if l1.nil?
 
+  # Create a new ListNode for calculation
   current = ListNode.new
+  # We use current for calculation and return result at the end because they will be the same instance
   result = current
+  # No carry at the begining
   carry = false
 
-  while l1 || l2 || carry
+  while !l1.nil? || !l2.nil? || carry
     sum = 0
     val1 = l1.nil? ? 0 : l1.val
     val2 = l2.nil? ? 0 : l2.val
     sum = carry ? val1 + val2 + 1 : val1 + val2
+    carry = sum > 9 ? true : false
+
+    l1 = l1.next
+    l2 = l2.next
 
     current.val = sum
-    current.next = current
+    current.next = ListNode.new
   end
 
   result
