75e3e6f7701de3b35395372744f6a48ea3395fc1
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-05 17:12:16 +0000

    completed logic and added psuedocode

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -21,21 +21,32 @@ def add_two_numbers(l1, l2)
   # We use current for calculation and return result at the end because they will be the same instance
   result = current
   # No carry at the begining
-  carry = false
+  carry = 0
 
-  while !l1.nil? || !l2.nil? || carry
+  # Run calculation when any of the l1.val, l2.val and carry exist
+  while !l1.nil? || !l2.nil? || carry == 1
     sum = 0
     val1 = l1.nil? ? 0 : l1.val
     val2 = l2.nil? ? 0 : l2.val
-    sum = carry ? val1 + val2 + 1 : val1 + val2
-    carry = sum > 9 ? true : false
+    sum = val1 + val2 + carry
+    carry = sum / 10
+    sum = sum % 10
 
-    l1 = l1.next
-    l2 = l2.next
+    # Check if l1 and l2 is already nil or not
+    l1 = l1.nil? ? nil : l1.next
+    l2 = l2.nil? ? nil : l2.next
 
+    # Update current.val to sum
     current.val = sum
-    current.next = ListNode.new
+    # Check if current is the last node or not
+    current.next = l1.nil? && l2.nil? ? nil : ListNode.new
   end
 
   result
 end
+
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+
+# Input: l1 = [7], l2 = [8]
+# Output: [5,1]
